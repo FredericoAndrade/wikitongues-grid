bb134031f641a838c8a3aa321ded2c51c9057498
Fredward <faandrade@gmail.com>
2020-06-15 22:51:54 -0400

    try to fix node heroku port

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = express();
 var path = require("path");
 var batch = require("./static/assets/js/batch.js");
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
   app.use(express.static(__dirname + '/static'));
 });
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('App listening on http://localhost:3000!');
 });
